427a4e166935b88e62f7673cd657c0c9ab314874
Igor Loncar <igor023@gmail.com>
2020-04-29 17:02:03 +0200

    on assigning types to get/post requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { map } from "rxjs/operators";
+import { Post } from "./post";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
+    console.log(postData);
+
     // Send Http request
     this.http
-      .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe(responseData => {
+      .post("https://ng2020-http.firebaseio.com/posts.json", postData)
+      .subscribe((responseData) => {
         console.log(responseData);
       });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    // Send Http request
+    this.http
+      .get("https://ng2020-http.firebaseio.com/posts.json")
+      .pipe(
+        map((responseData: { [key: string]: Post }) => {
+          console.log("responseData: ", responseData);
+
+          const postsArray = [];
+
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+
+          console.log("posts array: ", postsArray);
+        })
+      )
+      .subscribe((posts) => {
+        console.log(posts);
+      });
   }
 }
